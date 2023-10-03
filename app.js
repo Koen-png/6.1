68fc50f03ec165442b6650bf2d5f478ca437ea9f
Koen-png <dekoenduin@gmail.com>
2023-10-03 13:11:26 +0200

    yes

---
+++ app.js
@@ -0,0 +1,5 @@
+function changecolor(){
+    document.getElementById("thingamabob").innerHTML = "badaboom"
+    document.getElementById("thingamabob").style.color = "purple"
+    console.log("badaboom")
+}
